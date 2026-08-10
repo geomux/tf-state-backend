756b97c75b38def7133b3997dcbefc4074a5844f
geomux <Henry.LawrenceGEO@gmail.com>
2026-08-09 19:55:39 -0700

    feat(variables.tf): added project_name variable. Current variables for main.tf in this commit are project_name and aws_reigion. Both strings.

--- variables.tf
+++ variables.tf
@@ -1,7 +1,13 @@
 # variables.tf
 # Root Terraform variables config file for this IaC repo. Formats root main.tf script.
 
+
+variable "project_name" {
+  description = "NAME_PROJECT_HERE" # update accordingly
+  type = string
+}
+
 variable "aws_region" {
-  description = "us-east-1"
+  description = "us-east-1" # update accordingly
   type        = string
 }
