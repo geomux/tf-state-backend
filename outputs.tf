432af76250a032369919a435e142fd90fb183edb
geomux <Henry.LawrenceGEO@gmail.com>
2026-08-12 01:14:49 -0700

    feat(outputs.tf): added outputs for state bucket name and region.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
 # outputs.tf
 # Root Terraform outputs config file for this IaC repo. Holds provisioned resource values after apply is run.
 
+output "state_bucket_name" {
+  description = "Name of the S3 bucket holding remote state file."
+  value       = aws_s3_bucket.state.id
+  sensitive   = false
+}
+
+output "state_bucket_region" {
+  description = "Region the state bucket lives in."
+  value      = aws_s3_bucket.state.region
+  sensitive  = false
+}
